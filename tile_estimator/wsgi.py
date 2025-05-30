3d6c5fcc93f6ae0b68b00c70d75602ff2cb6174a
kwarteng silas <silaskwarteng17@gmail.com>
2025-05-30 10:41:49 +0000

    new one is in for me wooooo

--- tile_estimator/wsgi.py
+++ tile_estimator/wsgi.py
@@ -1,17 +1,18 @@
 import os
 import traceback
 import sys
-
+import logging # Add this line
 from django.core.wsgi import get_wsgi_application
 
+logger = logging.getLogger(__name__) # Add this line
+
 try:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tile_estimator.settings') # This should be 'tile_estimator.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tile_estimator.settings')
+    logger.info("Starting get_wsgi_application()") # Add this
     application = get_wsgi_application()
+    logger.info("Finished get_wsgi_application()") # Add this
 except Exception as e:
-    # Log the full traceback to stderr
     print("--- Django WSGI Application Load Error ---", file=sys.stderr)
     traceback.print_exc(file=sys.stderr)
     print("--- End of Error ---", file=sys.stderr)
-
-    # Re-raise the exception so Gunicorn still knows the worker failed
     raise
