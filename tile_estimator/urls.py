08799eb9c4ff4db3812f8de23891632110620e57
kwarteng silas <silaskwarteng17@gmail.com>
2025-05-30 06:23:09 +0000

    new one is app for

--- tile_estimator/urls.py
+++ tile_estimator/urls.py
@@ -6,9 +6,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+from django.urls import path
+
+
+def health_check(request):
+    return HttpResponse("OK")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', health_check),
     path('api/admin/', include('admin_api.urls')),
     path('api/user/', include('accounts.urls')),
     path('api/estimates/', include('estimates.urls')),
